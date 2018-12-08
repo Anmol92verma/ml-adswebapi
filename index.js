c86ca78288ddf983fa16cf8601f2b5e8c57ec3d3
Anmol Verma <anmol.verma4@gmail.com>
2018-12-09 00:34:31 +0530

    category and subcategory from list of options

--- index.js
+++ index.js
@@ -2,24 +2,74 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
+var RSVP = require("rsvp");
 var database;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+var categories;
+var subcategories;
+
+function getCategories() {
+  return new RSVP.Promise(function(resolve, reject) {
+    database
+      .collection("categories")
+      .find()
+      .toArray(function(err, results) {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          console.log(results);
+          categories = results;
+          resolve(results);
+        }
+      });
+  });
+}
+
+function getSubcategories() {
+  return new RSVP.Promise(function(resolve, reject) {
+    database
+      .collection("subcategories")
+      .find()
+      .toArray(function(err, results) {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          console.log(results);
+          subcategories = results;
+          resolve(results);
+        }
+      });
+  });
+}
+
 app.get("/", (req, res) => {
-  database
-    .collection("categories")
-    .find()
-    .toArray(function(err, results) {
-      if (err) return console.log(err);
-      // renders index.ejs
-      res.render("index.ejs", { categories: results });
+  getCategories()
+    .then(function(categories) {
+      return getSubcategories();
+    })
+    .then(function(subcategories) {
+      res.render("index.ejs", {
+        categories: categories,
+        subcategories: subcategories
+      });
     });
 });
 
 app.post("/addCategories", (req, res) => {
   database.collection("categories").save(req.body, (err, result) => {
+    if (err) return console.log(err);
+    console.log("saved to database");
+    res.redirect("/");
+  });
+});
+
+app.post("/addSubCategories", (req, res) => {
+  database.collection("subcategories").save(req.body, (err, result) => {
     if (err) return console.log(err);
     console.log("saved to database");
     res.redirect("/");
